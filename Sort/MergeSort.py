3b6a588a9ac515ad29c8e3658f131f6a4b4ffad9
AminGeng <gengamin@163.com>
2020-12-08 09:44:02 +0800

    添加了详细的注释, 补充了测试用时
    方便详细理解每种排序算法, 了解各个排序算法效率

--- Sort/MergeSort.py
+++ Sort/MergeSort.py
@@ -1,30 +1,55 @@
-#  稳定排序
+# 归并排序
+# 原理: 二分列表, 分别排好序两个子列表, 然后运行merge方法将两个排好序的子列表归并为排好序的列表
+# 原理续: 对子列表排序也是用二路归并策略, 所以merge_sort主体中需要用到递归
+# 稳定排序, 非原址
+# 时间复杂度: NlgN
 import random
+import time
 
 
-def merge(x, p, q, r):
-    list_l = x[p:q]
-    list_m = x[q:r + 1]
-    i, j = 0, 0
+# 将x[index_head1:index_head2]和x[index_head2:index_tail + 1]归并为排好序的列表x[index_head1:index_tail + 1]
+# 运行条件: x[index_head1:index_head2]和x[index_head2:index_tail + 1]已经分别排好序
+def merge(list_x, index_head1, index_head2, index_tail):
+    # 复制x[index_head1:index_head2]和x[index_head2:index_tail + 1], 因为归并时会改变x
+    list_l = list_x[index_head1:index_head2]
+    list_r = list_x[index_head2:index_tail + 1]
+    # 在list_l和list_r的尾部插入哑元素infinite, 为了在归并时省去判断是否搜到列表尾.
     list_l.append(float('inf'))
-    list_m.append(float('inf'))
-    for k in range(r - p + 1):
-        if list_m[i] < list_l[j]:
-            x[p + k] = list_m[i]
-            i += 1
+    list_r.append(float('inf'))
+    # index_list_l是list_l的位置指针, index_list_r是list_r的位置指针
+    index_list_l, index_list_r = 0, 0
+    # 归并过程
+    # 第k轮, 判断list_l[index_list_l]与list_r[index_list_r]的大小, 将小的放到x[index_head1 + k]的位置, 然后较小的位置指针后移1位
+    for k in range(index_tail - index_head1 + 1):
+        if list_r[index_list_r] < list_l[index_list_l]:
+            list_x[index_head1 + k] = list_r[index_list_r]
+            index_list_r += 1
         else:
-            x[p + k] = list_l[j]
-            j += 1
+            list_x[index_head1 + k] = list_l[index_list_l]
+            index_list_l += 1
 
 
-def merge_sort(x, p, q):
-    if q - p > 0:
-        t = (p + q) // 2
-        merge_sort(x, p, t)
-        merge_sort(x, t + 1, q)
-        merge(x, p, t + 1, q)
+# 归并排序list_x[index_head: index_tail+1]
+def merge_sort(list_x, index_head, index_tail):
+    # 归并启动条件 list_x[index_head: index_tail+1]中的元素个数>1: 元素个数=index_tail-index_head+1
+    if index_tail - index_head > 0:
+        # 精髓所在
+        # 二分列表为两个子列, 分别排好序, 然后运行merge方法, 归并排好序的两部分列表
+        # 子列表排序也采用二路归并策略, 所以用到递归
+        index_m = (index_head + index_tail) // 2
+        merge_sort(list_x, index_head, index_m)
+        merge_sort(list_x, index_m + 1, index_tail)
+        merge(list_x, index_head, index_m + 1, index_tail)
 
 
-x = [random.randint(1, 1000) for _ in range(100)]
-merge_sort(x, 0, len(x) - 1)
-print(x)
+# 测试部分
+# 与系统方法sorted()的结果进行比对
+# 测试用时: 4.455240488052368, 快
+start = time.time()
+for i in range(1000):
+    A = [random.randint(1, 100) for _ in range(random.randint(1, 2000))]
+    B = sorted(A)
+    merge_sort(A, 0, len(A) - 1)
+    if A != B:
+        print('error')
+print(time.time() - start)
--- Sort/RadixSort.py
+++ Sort/RadixSort.py
@@ -1,28 +1,51 @@
+# 基数排序
+# 此文以固定位数的整数为例
+# 原理: 从个位开始, 从低到高位, 依次运行counting_sort按该位大小(稳定的)对列表进行排序
+# 稳定排序, 非原址
+# 时间复杂度O(D(N+R)): D是整数的位数, N:列表元素总数, R:每一位的数字数, 十进制就是0~9共10个
+# 为了方便, 我只编写了对四位数以每一位为基进行排序, 其他情况对代码进行相应更改即可
 import random
+import time
 
 
-def counting_sort(list_a, n, k):
+# 在计数排序的基础上进行了稍微的更改, 按位从低到高, 每次依照第digit_i位大小对list_a排序, 具体原理参见Counting_Sort.py
+# list_a[index_i] // (10 ** digit_i) % 10 取list_a[index_i]得第digit_i位数(个位:0, 十位:1, ...)
+# digit_i: 排序的位, [k1, k2]: 整数所在区间
+def counting_sort(list_a, digit_i, k1, k2):
     list_b = list_a[:]
     dict_c = {}
-    for i in range(k + 1):
-        dict_c[i] = 0
-    for i in range(len(list_a)):
-        dict_c[int(list_a[i][n])] += 1
-    for i in range(1, k + 1):
-        dict_c[i] += dict_c[i - 1]
-    for i in range(len(list_a) - 1, -1, -1):  # insure stability
-        list_b[int(dict_c[int(list_a[i][n])]) - 1] = list_a[i]
-        dict_c[int(list_a[i][n])] -= 1
+    for number_i in range(k1, k2 + 1):
+        dict_c[number_i] = 0
+    for index_i in range(len(list_a)):
+        dict_c[list_a[index_i] // (10 ** digit_i) % 10] += 1
+    for number_i in range(k1 + 1, k2 + 1):
+        dict_c[number_i] += dict_c[number_i - 1]
+    for index_i in range(len(list_a) - 1, -1, -1):
+        value_i_i = list_a[index_i] // (10 ** digit_i) % 10
+        list_b[dict_c[value_i_i] - 1] = list_a[index_i]
+        dict_c[value_i_i] -= 1
     return list_b
 
 
-A = [str(random.randint(100, 999)) for _ in range(500)]
-
-
-def radix_sort(list_a, d):
-    for i in range(d - 1, -1, -1):
-        list_a = counting_sort(list_a, i, 9)
+# 从个位开始, 从低到高位, 依次运行counting_sort按该位大小(稳定的)对列表进行排序
+# digits: 指定数的位数
+def radix_sort(list_a, digits):
+    for digit_i in range(digits):
+        list_a = counting_sort(list_a, digit_i, 0, 9)
     return list_a
 
 
-print(radix_sort(A, 3))
+# 测试部分
+# 与系统方法sorted()的结果进行比对
+# 测试用时: 三位数:3.2386910915374756, 四位数:4.086498498916626, 五位数5.053367853164673
+# 随位数线性增长, 很快
+# 如需更改位数, 请更改标记两处
+start = time.time()
+for i in range(1000):
+    # random.randint(10000, 99999), 指定是5位数
+    A = [random.randint(10000, 99999) for _ in range(random.randint(1, 2000))]
+    B = sorted(A)
+    # radix_sort(A, 5), 必须填写指定位数
+    if radix_sort(A, 5) != B:
+        print('error')
+print(time.time() - start)
