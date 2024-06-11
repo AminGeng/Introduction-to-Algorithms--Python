46b9108ee149ff42b7705aaa7fee388a2e373334
Amin <gengamin@163.com>
2024-06-11 15:31:35 +0800

    use list for count

--- Sort/CountingSort.py
+++ Sort/CountingSort.py
@@ -1,48 +1,31 @@
-# 计数排序
-# 原理: 计数排序需要提前知道列表中会出现哪些数, 记出现的数的总数为K, 列表的总容量为N
-# 原理续: 先检索一遍列表, 记录各个数出现的次数, 然后计算出各个数该出现的最后位置(从小到大次数累加)
-# 原理续: 新建一个等大列表, 从后往前检索原列表(稳定性), 将该数放到它的实际排序位置处(放到该出现的最后位置, 然后该数出现的最后位置-1)
-# 稳定排序, 非原址
-# 时间复杂度: O(K+N)
-# 为了方便, 我只编写了可能出现的数是[k1, k2]区间的整数的列表, 其他情况我简单脑补可以通过字典的形式解决
-import random
-import time
-
+"""
+计数排序
+稳定 
+空间复杂度: O(K+N)
+时间复杂度: O(K+N)
+原理: 
+    假设N个元素都是[0,K]区间的整数. 
+    创建列表L_K(size=K+1), 先检索一遍原列表, 记录各个数出现的次数至L_K, 然后计算出各个数该出现的最后位置(L_K[i+1]+=L_K[i] i in [0, K-1])
+    新建一个等大列表L_N, 从后往前检索原列表(稳定性), 将该数放到它的实际排序位置处(L_N[L_K[i]++])
+"""
 
 # k1, k2是整数, list_a中只出现[k1, k2]区间的整数
-def counting_sort(list_a, k1, k2):
+def counting_sort(list_a):
+    if (len(list_a)<2): return
     # list_b用来存放最后排好序的列表
-    list_b = list_a[:]  # 复制比创建等大的新列表速度快, 比如创建:list_b = [0 for _ in range(len(list_a))]
-    # 用来存放每个数和每个数该出现的最后位置: key:数, value:该数出现的最后位置
-    dict_c = {}
-    # 为每个可能出现在列表中的数创建key
-    for number_i in range(k1, k2 + 1):
-        dict_c[number_i] = 0
-    # 计算每个数出现的次数
-    for index_i in range(len(list_a)):
-        dict_c[list_a[index_i]] += 1
-    # 计数每个数出现的最后位置
-    for number_i in range(k1 + 1, k2 + 1):
-        dict_c[number_i] += dict_c[number_i - 1]
-    # 确保稳定性
-    # 从后往前检索原列表(稳定性), 将该数放到它的实际排序位置处(放到该出现的最后位置, 然后该数出现的最后位置-1)
-    for index_i in range(len(list_a) - 1, -1, -1):
-        list_b[dict_c[list_a[index_i]] - 1] = list_a[index_i]
-        dict_c[list_a[index_i]] -= 1
-    return list_b
+    k1 = min(list_a)
+    k2 = max(list_a)
 
+    list_b = list_a[:]  # 复制列表
+    # 用来存放每个数和每个数该出现的最后位置
+    list_k = [0]*(k2-k1+1)
+    # 记录每个元素出现的次数
+    for e in list_a:
+        list_k[e-k1] += 1
+    # 计算每个元素出现的最后位置+1
+    for i in range(0, k2-k1):
+        list_k[i+1] += list_k[i]
+    for e in list_b[::-1]:
+        list_a[list_k[e-k1]-1] = e
+        list_k[e-k1] -= 1
 
-# 测试部分
-# 与系统方法sorted()的结果进行比对
-# 测试用时: [-100, 100]:1.535020351409912, [-1000, 1000]:2.2114148139953613
-# 最快的啦
-# 如需更改范围, 请更改标记两处
-start = time.time()
-for i in range(1000):
-    # random.randint(-1000, 1000), 指定整数范围[-1000, 1000]
-    A = [random.randint(-1000, 1000) for _ in range(random.randint(1, 2000))]
-    B = sorted(A)
-    # counting_sort(A, -1000, 1000), 必须与指定一致或包含指定区间
-    if counting_sort(A, -1000, 1000) != B:
-        print('error')
-print(time.time() - start)
